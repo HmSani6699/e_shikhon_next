9aad65fe57c1b35911485419db4ef8f81e422e19
H.M Sadiq <hmsadikurrahman120@gmail.com>
2024-02-11 15:00:45 +0600

    added new file

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 "use client"
 import React, { FC, useState } from "react"
+import Heading from "./utils/Heading"
 
 interface Props{ }
 
 const Page: FC<Props> = (props) => {
   return (
     <div>
-
+      <Heading title="e-Shikhon" description="Learning" keywords="React,Fron end ," />
     </div>
   )
 }
