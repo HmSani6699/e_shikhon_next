f8778c87e7b1b16f98c59310570d402cf342cf57
H.M Sadiq <hmsadikurrahman120@gmail.com>
2024-02-11 16:23:34 +0600

    added navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
 "use client"
 import React, { FC, useState } from "react"
 import Heading from "./utils/Heading"
+import Header from "./component/Header"
 
 interface Props{ }
 
 const Page: FC<Props> = (props) => {
+  const [open, setOpen] = useState(false)
+  const [activeItem,setActiveItem]=useState(0)
+
   return (
     <div>
       <Heading title="e-Shikhon" description="Learning" keywords="React,Fron end ," />
+      <Header
+        open={open}
+        setOpen={setOpen}
+        activeItem={activeItem}
+      />
     </div>
   )
 }
