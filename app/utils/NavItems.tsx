bfc08c08faceae3c420590bee4763b2286a92db8
H.M Sadiq <hmsadikurrahman120@gmail.com>
2024-02-12 15:44:53 +0600

    complete nevbar

--- app/utils/NavItems.tsx
+++ app/utils/NavItems.tsx
@@ -50,19 +50,32 @@ const NavItems: React.FC<Props> = ({ activeItem, isMobile }) => {
       {isMobile && (
         <div className="800px:hidden mt-5">
           <div className="w-full text-center py-6">
+            <Link
+              href={"/"}
+              className={`text-[25px] font-Poppins font-[500] text-black dark:text-white`}
+              passHref
+            >
+              e-Shikhon
+            </Link>
+          </div>
+          <div className="">
             {navItemsData &&
               navItemsData?.map((i, index) => (
-                <Link href={`${i.url}`} key={index} passHref>
-                  <span
-                    className={`${
-                      activeItem === index
-                        ? "dark:text-[#37a39a] text-[crimson]"
-                        : "dark:text-white text-black"
-                    } text-[18px] px-6 font-Poppins font-[400]`}
-                  >
-                    {i.name}
-                  </span>
-                </Link>
+                <>
+                  <Link href={`${i.url}`} key={index} passHref>
+                    <span
+                      className={`${
+                        activeItem === index
+                          ? "dark:text-[#37a39a] text-[crimson]"
+                          : "dark:text-white text-black"
+                      } text-[18px] px-6  font-Poppins font-[400]`}
+                    >
+                      {i.name}
+                    </span>
+                  </Link>
+                  <br />
+                  <br />
+                </>
               ))}
           </div>
         </div>
