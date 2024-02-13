d13c5e8bbf8d45b830498052e42f52a72e76a357
H.M Sadiq <hmsadikurrahman120@gmail.com>
2024-02-13 14:50:25 +0600

    added banner img

--- app/component/Route/Hero.tsx
+++ app/component/Route/Hero.tsx
@@ -1,31 +1,25 @@
+import Image from "next/image";
 import React, { FC } from "react";
+import { BiSearch } from "react-icons/bi";
 
 type Props = {};
 
 const Hero: FC<Props> = (props) => {
   return (
-    <div className="w-full 1000px:flex items-center h-screen">
-      {/* left site content */}
-      <div className="lg:w-1/2">
-        <h2
-          className={`text-[25px] font-Josefin font-[500] text-black dark:text-white`}
-        >
-          Left site
-        </h2>
+    <div className="w-full 1000px:flex items-center">
+      <div className="absolute top-[100px] 1000px:top-[unset] 1500px:h-[700px] 1500px:w-[700px] 1100px:h-[600px] h-[50vh] w-[50vh] hero_animation rounded-full"></div>
+      <div className="1000px:w-[40%] flex 1000px:min-h-screen items-center justify-end pt-[70px] 1000px:pt-[0] z-10 ">
+        <Image
+          src={require("../../../public/banner-img-1.png")}
+          alt="banner img"
+          className="object-contain 1100px:max-w-[90%] w-[90%] 1500px:max-w-[85%] h-[auto] z-10 "
+        />
       </div>
-      {/* Right digte content */}
-      <div className="lg:w-1/2">
-        <h2
-          className={`text-[60px] font-Josefin font-[500] text-black dark:text-white`}
-        >
-          Improve Your Online Learning Experience Better Instantiy
+      <div className="1000px:w-[60%] flex flex-col items-center 1000px:mt-[0px] text-center 1000px:text-left mt-[150px]">
+        <h2 className="dark:text-white text-black text-[30px] px-3 w-full 1000px:text-[70px] font-Josefin py-2 1000px:leading-[75px] 1500px:w-[600px]  ">
+          {" "}
+          Imporve Your Online Learning Experience Better Instantly
         </h2>
-        <p
-          className={`text-[16px] font-Josefin font-[500] text-black dark:text-white`}
-        >
-          We have 40k+ Online courses & 500k+ Online registered student. Find
-          your desired Courses from them
-        </p>
       </div>
     </div>
   );
