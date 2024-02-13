1d2b2e711e07c7f0cf39ff4bbb06afbb44b4df39
H.M Sadiq <hmsadikurrahman120@gmail.com>
2024-02-13 15:06:17 +0600

    added search ber

--- app/component/Route/Hero.tsx
+++ app/component/Route/Hero.tsx
@@ -17,9 +17,25 @@ const Hero: FC<Props> = (props) => {
       </div>
       <div className="1000px:w-[60%] flex flex-col items-center 1000px:mt-[0px] text-center 1000px:text-left mt-[150px]">
         <h2 className="dark:text-white text-black text-[30px] px-3 w-full 1000px:text-[70px] font-Josefin py-2 1000px:leading-[75px] 1500px:w-[600px]  ">
-          {" "}
           Imporve Your Online Learning Experience Better Instantly
         </h2>
+        <br />
+        <p className="dark:text-[#edfff4] text-[#000000ac] font-Josefin font-[600] text-[18px] 1500px:!w-[55%] 1100px:!w-[78%] ">
+          We have 40k+ Online courses & 500K+ Online registered student. Find
+          your desired Courses from them.
+        </p>
+        <br />
+        <br />
+        <div className="1500px:w-[55%] 1100px:w-[78%] w-[90%] h-[50%] bg-transparent  relative ">
+          <input
+            type="search"
+            placeholder="Search Courses..."
+            className="bg-transparent border dark:border-none dark:bg-[#575757] dark:placeholder:text-[#ffffffdd] rounded-[5px] p-2 w-full h-full outline-none text-[#0000004e] dark:text-[#ffffffe6] text-[20px] font-[500] font-Josefin "
+          />
+          <div className="absolute flex items-center justify-center w-[50px] cursor-pointer h-[50px] right-0 top-0 bg-[#39c1f3] rounded-r-[5px]  ">
+            <BiSearch className="text-white" size={30} />
+          </div>
+        </div>
       </div>
     </div>
   );
