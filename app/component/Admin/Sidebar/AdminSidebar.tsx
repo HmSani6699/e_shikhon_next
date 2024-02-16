5cadb78fe45e3fffc4c37e07362e15d00fdf3258
H.M Sadiq <hmsadikurrahman120@gmail.com>
2024-02-16 19:10:46 +0600

    added admin title

--- app/component/Admin/Sidebar/AdminSidebar.tsx
+++ app/component/Admin/Sidebar/AdminSidebar.tsx
@@ -3,7 +3,11 @@ import React, { FC } from "react";
 type Props = {};
 
 const AdminSidebar: FC<Props> = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Added amin side bar</h1>
+    </div>
+  );
 };
 
 export default AdminSidebar;
