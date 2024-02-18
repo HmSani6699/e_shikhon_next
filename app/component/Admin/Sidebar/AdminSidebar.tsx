027ff892e65fc5561e6b1f49e5370f49c6e9ddf1
H.M Sadiq <hmsadikurrahman120@gmail.com>
2024-02-18 21:58:08 +0600

    added 1 box

--- app/component/Admin/Sidebar/AdminSidebar.tsx
+++ app/component/Admin/Sidebar/AdminSidebar.tsx
@@ -3,15 +3,140 @@ import { FC, useState, useEffect } from "react";
 import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
 import { Box, IconButton, Typography } from "@mui/material";
 import "react-pro-sidebar/dist/css/styles.css";
-import {} from "./Icon";
+import {
+  HomeOutlinedIcon,
+  ArrowForwardIosIcon,
+  ArrowBackIosIcon,
+  PeopleOutlinedIcon,
+  ReceiptOutlinedIcon,
+  BarChartOutlinedIcon,
+  MapOutlinedIcon,
+  GroupsIcon,
+  OndemandVideoIcon,
+  VideoCallIcon,
+  WebIcon,
+  QuizIcon,
+  WysiwygIcon,
+  ManageHistoryIcon,
+  SettingsIcon,
+  ExitToAppIcon,
+} from "./Icon";
 
-type Props = {};
+import avatarDefault from "../../../../public/avatar.svg";
+import { UseSelector, useSelector } from "react-redux";
+import Link from "next/link";
+import Image from "next/image";
+import { useTheme } from "next-themes";
 
-const AdminSidebar: FC<Props> = () => {
+interface itemProps {
+  title: string;
+  to: string;
+  icon: JSX.Element;
+  selected: string;
+  setSeclected: any;
+}
+
+const AdminSidebar: FC<itemProps> = ({
+  title,
+  to,
+  icon,
+  selected,
+  setSeclected,
+}) => {
   return (
-    <div>
-      <h1>Added amin side bar</h1>
-    </div>
+    <MenuItem
+      active={selected === title}
+      onClick={() => setSeclected(title)}
+      icon={icon}
+    >
+      <Typography className="!text-[16px] !font-Poppins  ">{title}</Typography>
+      <Link href={to} />
+    </MenuItem>
+  );
+};
+
+const Sidebar = () => {
+  const { user } = useSelector((state: any) => state.auth);
+  const [logOut, setLogOut] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [selected, setSelected] = useState("Dashboard");
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+  useEffect(() => setMounted(true), []);
+  if (!mounted) {
+    return null;
+  }
+  const logoutHandler = () => {
+    setLogOut(true);
+  };
+
+  return (
+    <Box
+      sx={{
+        "& .pro-sidebar-inner": {
+          background: `${
+            theme === "dark" ? "#111c41 !important" : "#fff !important"
+          }`,
+        },
+        "$ .pro-icon-wrapper": {
+          background: "transparent !important ",
+        },
+        "& .pro-inner-item:hover": {
+          color: "#6870fa !important",
+        },
+        "& .pro-menu-item.active": {
+          color: "#6870fa !important",
+        },
+        "& .pro-inner-item": {
+          padding: "5px 35px 5px 20px !important",
+        },
+        "& .pro-menu-item": {
+          color: "!bg-white dark:bg-[#111C43]",
+        },
+      }}
+    >
+      <ProSidebar
+        collapsed={isCollapsed}
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          height: "100vh",
+          width: isCollapsed ? "0%" : "16%",
+        }}
+      >
+        <Menu iconShape="square">
+          <MenuItem
+            onClick={() => setIsCollapsed(!isCollapsed)}
+            icon={isCollapsed ? <ArrowForwardIosIcon /> : undefined}
+            style={{
+              margin: "10px 0 20px 0",
+            }}
+          >
+            {!isCollapsed && (
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                ml="15px"
+              >
+                <Link href="/">
+                  <h3 className=" text-[25px] font-Poppins uppercase dark:text-white text-black ">
+                    e-Shikhon
+                  </h3>
+                </Link>
+                <IconButton
+                  onClick={() => setIsCollapsed(!isCollapsed)}
+                  className="inline-block"
+                >
+                  <ArrowBackIosIcon className="text-black dark:text-[#ffffffc1] " />
+                </IconButton>
+              </Box>
+            )}
+          </MenuItem>
+        </Menu>
+      </ProSidebar>
+    </Box>
   );
 };
 
