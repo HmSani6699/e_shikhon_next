b709ee1f59391a8f37ab87403072f33292c3e282
H.M Sadiq <hmsadikurrahman120@gmail.com>
2024-02-19 19:20:14 +0600

    added new code

--- app/component/Admin/Course/CourseInformation.tsx
+++ app/component/Admin/Course/CourseInformation.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { FC } from "react";
 
-const CourseInformation = () => {
+
+type Props = {
+  courseInfo: any;
+  setCourseInfo(courseInfo: any)=> void;
+  active: number;
+  setActive = (active: number) => void;
+};
+
+const CourseInformation: FC<Props> = ({ courseInfo, setCourseInfo, active, setActive }:Props) => {
   return <div></div>;
 };
 
