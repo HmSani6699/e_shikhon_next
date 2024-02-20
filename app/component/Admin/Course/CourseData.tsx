d64f007d30bdda548e6923953e388b28f3d5f91c
H.M Sadiq <hmsadikurrahman120@gmail.com>
2024-02-20 12:32:42 +0600

    added new page

--- app/component/Admin/Course/CourseData.tsx
+++ app/component/Admin/Course/CourseData.tsx
@@ -1,4 +1,7 @@
+import { styles } from "@/app/styles/style";
 import React, { FC } from "react";
+import AddCrileIcone from "@mui/icons-material/AddCircle";
+
 type Props = {
   benefits: { title: string }[];
   setBenefits: (benefits: { title: string }[]) => void;
@@ -15,7 +18,91 @@ const CourseData: FC<Props> = ({
   active,
   setActive,
 }) => {
-  return <div></div>;
+  const handleBenefitChange = (index: number, value: any) => {
+    const updateBenefits = [...benefits];
+    updateBenefits[index].title = value;
+    setBenefits(updateBenefits);
+  };
+
+  const handleAddBenefit = () => {
+    setBenefits([...benefits, { title: "" }]);
+  };
+
+  const handlePrerequisitesChange = (index: number, value: any) => {
+    const updatePrerequisites = [...prerequisites];
+    updatePrerequisites[index].title = value;
+    setPrerequisites(updatePrerequisites);
+  };
+
+  const handleAddprerequisites = () => {
+    setPrerequisites([...prerequisites, { title: "" }]);
+  };
+
+  const prevButton = () => {};
+  const handleOptions = () => {};
+
+  return (
+    <div className="w-[80%] m-auto mt-24 block">
+      <div>
+        <label className={`${styles.label} text-[20px]`} htmlFor="email">
+          What are the benefits for students in this course
+        </label>
+        <br />
+        {benefits.map((benefit: any, index: number) => (
+          <input
+            type="text"
+            key={index}
+            name="Benefit"
+            placeholder="you will be able to build a full stack LMS Platform"
+            required
+            className={`${styles.input} my-2`}
+            value={benefit.title}
+            onChange={(e) => handleBenefitChange(index, e.target.value)}
+          />
+        ))}
+        <AddCrileIcone
+          style={{ margin: "10px 0px", cursor: "pointer", width: "30px" }}
+          onClick={handleAddBenefit}
+        />
+      </div>
+      <div>
+        <label className={`${styles.label} text-[20px]`} htmlFor="email">
+          What are the prerequisites for starting this course ?
+        </label>
+        <br />
+        {prerequisites.map((prerequisites: any, index: number) => (
+          <input
+            type="text"
+            key={index}
+            name="Prerequisites"
+            placeholder="you need basic kanolwdeg of MERN stack"
+            required
+            className={`${styles.input} my-2`}
+            value={prerequisites.title}
+            onChange={(e) => handlePrerequisitesChange(index, e.target.value)}
+          />
+        ))}
+        <AddCrileIcone
+          style={{ margin: "10px 0px", cursor: "pointer", width: "30px" }}
+          onClick={handleAddprerequisites}
+        />
+      </div>
+      <div className="w-full flex items-center justify-between ">
+        <div
+          className="w-full 800px:w-[180px] flex items-center justify-center h-[40px] bg-[#37a39a] text-center text-white rounded mt-8 cursor-pointer "
+          onClick={() => prevButton()}
+        >
+          Prev
+        </div>
+        <div
+          className="w-full 800px:w-[180px] flex items-center justify-center h-[40px] bg-[#37a39a] text-center text-white rounded mt-8 cursor-pointer "
+          onClick={() => handleOptions()}
+        >
+          Next
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CourseData;
