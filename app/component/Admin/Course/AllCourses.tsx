683f883b13c2eec4feefe982c62bbcda9d287be0
H.M Sadiq <hmsadikurrahman120@gmail.com>
2024-02-24 20:39:56 +0600

    update code

--- app/component/Admin/Course/AllCourses.tsx
+++ app/component/Admin/Course/AllCourses.tsx
@@ -1,6 +1,43 @@
 import React from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { Box, Button } from "@mui/material";
+import { AiOutlineDelete } from "react-icons/ai";
+import { useTheme } from "next-themes";
+
+type Prorps = {};
 
 const AllCourses = () => {
+  const { theme, setTheme } = useTheme();
+
+  const columns = [
+    { field: "id", headerName: "ID", flex: 0.5 },
+    { field: "title", headerName: "Course Title", flex: 0.5 },
+    { field: "ratings", headerName: "Ratings", flex: 0.5 },
+    { field: "purchased", headerName: "Purchased", flex: 0.5 },
+    { field: "created_at", headerName: "Created At", flex: 0.5 },
+    {
+      field: "",
+      headerName: "Delete",
+      flex: 0.2,
+      renderCell: (params: any) => {
+        return (
+          <>
+            <Button>
+              <AiOutlineDelete
+                className="dark:text-white text-black"
+                size={20}
+              />
+            </Button>
+          </>
+        );
+      },
+    },
+  ];
+
+  const rows = [
+    { id: "123", title: "React", purchased: "30", created_at: "12/12/12" },
+  ];
+
   return <div></div>;
 };
 
--- redux/features/api/apiSlice.ts
+++ redux/features/api/apiSlice.ts
@@ -1,4 +1,4 @@
-import {createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { url } from "inspector";
 import { userLoggedIn } from "../auth/authSlice";
 
