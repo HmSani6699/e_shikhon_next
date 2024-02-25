3a9512764e2b93ae144196d95658711222cd7408
H.M Sadiq <hmsadikurrahman120@gmail.com>
2024-02-25 15:20:32 +0600

    added new code

--- app/component/Admin/Course/AllCourses.tsx
+++ app/component/Admin/Course/AllCourses.tsx
@@ -38,7 +38,66 @@ const AllCourses = () => {
     { id: "123", title: "React", purchased: "30", created_at: "12/12/12" },
   ];
 
-  return <div></div>;
+  return (
+    <div className="mt-[120px]">
+      <Box m="20px">
+        <Box
+          m="40px 0 0 0 "
+          height="80vh"
+          sx={{
+            "& .MuiDataGrid-root": {
+              border: "none",
+              outline: "none",
+            },
+            "& .css-pqjvzy-MuiSvgIcon-root-MuiSelect-icon": {
+              color: theme === " dark" ? "#fff" : "#000",
+            },
+            "& .MuiDataGrid-sortIcon": {
+              color: theme === " dark" ? "#fff" : "#000",
+            },
+            "& .MuiDataGrid-row": {
+              color: theme === " dark" ? "#fff" : "#000",
+              borderBottom:
+                theme === "dark"
+                  ? "1px solid #ffffff30 !important"
+                  : "1px solid #ccc !important",
+            },
+            "& .MuiTablePagination-root": {
+              color: theme === " dark" ? "#fff" : "#000",
+            },
+            "& .MuiDataGrid-cell": {
+              borderBottom: "none",
+            },
+            "& .name-column--cell": {
+              color: theme === " dark" ? "#fff" : "#000",
+            },
+            "& .MuiDataGrid-columnHeaders": {
+              backgroundColor: theme === " dark" ? "#3e4396" : "#A4A9FC",
+              borderBottom: "nonoe",
+              color: theme === " dark" ? "#fff" : "#000",
+            },
+            "& .MuiDataGrid-virtualScroller": {
+              backgroundColor: theme === " dark" ? "#1F2A40" : "#F2F0F0",
+            },
+            "& .MuiDataGrid-footerContainer": {
+              color: theme === " dark" ? "#fff" : "#000",
+              borderTop: "nonoe",
+              backgroundColor: theme === " dark" ? "#3e4396" : "#A4A9FC",
+            },
+            "& .MuiCheckbox-root": {
+              color:
+                theme === " dark" ? "#b7ebde !important" : "#000 !important",
+            },
+            "& .MuiCheckbox-toolbarContainer .MuiButton-text": {
+              color: "#fff !important",
+            },
+          }}
+        >
+          <DataGrid checkboxSelection rows={rows} columns={columns} />
+        </Box>
+      </Box>
+    </div>
+  );
 };
 
 export default AllCourses;
